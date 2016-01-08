1ffc54e35fe9115d582b5bf24c942b84d7f373fe
Nicola Montecchio <nicola.montecchio@gmail.com>
2016-01-08 10:47:51 -0500

    web mode for ngt files

--- init.el
+++ init.el
@@ -199,6 +199,7 @@
   (setq web-mode-markup-indent-offset 2))
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ngt?\\'" . web-mode))
 
 ;; scala
 (defun ss ()
