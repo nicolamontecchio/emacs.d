13b524bee29beef164c84e7d6770122f9885dc3e
nicola montecchio <nicola.montecchio@gmail.com>
2016-05-18 07:13:20 -0400

    face

--- init.el
+++ init.el
@@ -244,10 +244,3 @@
 (setq magit-last-seen-setup-instructions "1.4.0")
 
 (require 'wgrep)
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :weight medium :height 130 :width normal :family "source code pro")))))
