387ad33ac689ef1a5eb715130dd98631ed3906d5
Nicola Montecchio <nicola.montecchio@gmail.com>
2018-09-18 07:49:44 +0200

    thesaurus keybinding

--- init.el
+++ init.el
@@ -320,6 +320,20 @@
 (add-hook 'LilyPond-mode-hook (lambda () (turn-on-font-lock)))
 
 
+;; latex
+
+;; (require 'browse-url) ; part of gnu emacs
+(defun lookup-thesaurus ()
+  (interactive)
+  (let (word)
+    (setq word
+          (if (use-region-p)
+              (buffer-substring-no-properties (region-beginning) (region-end))
+            (current-word)))
+    (setq word (replace-regexp-in-string " " "_" word))
+    (browse-url (concat "https://www.thesaurus.com/browse/" word))))
+(global-set-key (kbd "<f8>")     'lookup-thesaurus)
+
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -345,7 +359,7 @@
     ("#336c6c" "#205070" "#0f2050" "#806080" "#401440" "#6c1f1c" "#6b400c" "#23733c")))
  '(package-selected-packages
    (quote
-    (lsp-rust ensime 2048-game greymatters-theme green-phosphor-theme faff-theme eziam-theme autumn-light-theme anti-zenburn-theme doneburn-theme dakrone-light-theme string-inflection solarized-theme dumb-jump atomic-chrome yasnippet yaml-mode wrap-region window-number wgrep-ag web-mode typescript-mode toml-mode telephone-line switch-window sublime-themes smex scala-mode sbt-mode rjsx-mode rg racket-mode racer protobuf-mode projectile powerline paredit pallet oceanic-theme mc-extras material-theme markdown-mode magit kaolin-themes julia-mode json-mode highlight-symbol hi2 groovy-mode go-mode geiser flatland-theme fill-column-indicator expand-region dockerfile-mode direx danneskjold-theme company-irony company-ghc cmake-mode clang-format cider avy atom-one-dark-theme)))
+    (mw-thesaurus lsp-rust ensime 2048-game greymatters-theme green-phosphor-theme faff-theme eziam-theme autumn-light-theme anti-zenburn-theme doneburn-theme dakrone-light-theme string-inflection solarized-theme dumb-jump atomic-chrome yasnippet yaml-mode wrap-region window-number wgrep-ag web-mode typescript-mode toml-mode telephone-line switch-window sublime-themes smex scala-mode sbt-mode rjsx-mode rg racket-mode racer protobuf-mode projectile powerline paredit pallet oceanic-theme mc-extras material-theme markdown-mode magit kaolin-themes julia-mode json-mode highlight-symbol hi2 groovy-mode go-mode geiser flatland-theme fill-column-indicator expand-region dockerfile-mode direx danneskjold-theme company-irony company-ghc cmake-mode clang-format cider avy atom-one-dark-theme)))
  '(pdf-view-midnight-colors (quote ("#ffffff" . "#444444")))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
