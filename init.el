c9cbbd5fdc2cfad14759a036581d625475ef50cd
Nicola Montecchio <nicola.montecchio@gmail.com>
2021-11-16 21:46:47 +0100

    new default theme

--- init.el
+++ init.el
@@ -167,7 +167,8 @@
 (if (display-graphic-p)
     (progn
       ;; themes
-      (setq pretty-themes '(doom-opera
+      (setq pretty-themes '(doom-vibrant
+			    doom-opera
 			    doom-spacegrey
 			    doom-peacock
 			    sanityinc-tomorrow-eighties
@@ -368,7 +369,47 @@
 
 ;; markdown preview -- NB needs pandoc installed
 (custom-set-variables
- '(markdown-command "/usr/local/bin/pandoc"))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#323334" "#C16069" "#A2BF8A" "#ECCC87" "#80A0C2" "#B58DAE" "#86C0D1" "#eceff4"])
+ '(custom-safe-themes
+   '("0d01e1e300fcafa34ba35d5cf0a21b3b23bc4053d388e352ae6a901994597ab1" default))
+ '(exwm-floating-border-color "#181818")
+ '(fci-rule-color "#525252")
+ '(highlight-tail-colors ((("#3d413c") . 0) (("#3a4143") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#000000" "#80A0C2"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#000000" "#A2BF8A"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#000000" "#3f3f3f"))
+ '(markdown-command "/usr/local/bin/pandoc")
+ '(objed-cursor-color "#C16069")
+ '(pdf-view-midnight-colors (cons "#eceff4" "#323334"))
+ '(rustic-ansi-faces
+   ["#323334" "#C16069" "#A2BF8A" "#ECCC87" "#80A0C2" "#B58DAE" "#86C0D1" "#eceff4"])
+ '(vc-annotate-background "#323334")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#A2BF8A")
+    (cons 40 "#bac389")
+    (cons 60 "#d3c788")
+    (cons 80 "#ECCC87")
+    (cons 100 "#e3b57e")
+    (cons 120 "#da9e75")
+    (cons 140 "#D2876D")
+    (cons 160 "#c88982")
+    (cons 180 "#be8b98")
+    (cons 200 "#B58DAE")
+    (cons 220 "#b97e97")
+    (cons 240 "#bd6f80")
+    (cons 260 "#C16069")
+    (cons 280 "#a0575e")
+    (cons 300 "#804f54")
+    (cons 320 "#5f4749")
+    (cons 340 "#525252")
+    (cons 360 "#525252")))
+ '(vc-annotate-very-old-color nil))
 
 ;; deft
 (setq deft-directory "~/Dropbox/zk")
